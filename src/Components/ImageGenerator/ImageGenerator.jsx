f3ac988aa2ebf56441fa3e8f9a1592938775b2b6
Niyathi Mariya George <nmg2881@gmail.com>
2024-05-15 00:44:45 +0530

    added html and css

---
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './ImageGenerator.css'
+import default_image from '../Assets/default_image.svg'
+const ImageGenerator = () => {
+  return (
+    <div className='ai-image-generator'>
+        <div className='header'>AI image <span>generator</span></div>
+        <div className="img-loading">
+            <div className="image"><img src={default_image} alt=""/></div>
+        </div>
+        <div className="search-box">
+            <input type="text" className='search-input' placeholder="Describe what you want to see"/>
+            <div className="generate-btn">Generate</div>
+        </div>
+    </div>
+  )
+}
+
+export default ImageGenerator
